91e5617dce55523403670d1206408de8d6e40096
jorge luis garcia <george92_1003@hotmail.com>
2013-09-03 23:20:26 -0500

    primer cambio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Miapppost::Application.config.secret_key_base = '074cea054545a456f3fb8005e5bc2c1e36b074b7455fc2ede59d8bf4bf43ad2c7cdfd86f89a888f268f49eebd84133318a4bf07f024df31f79ed009195e15e2a'
